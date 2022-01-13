20cd3562762ad18351f285d5011b2793840ba28d
Andrea Grossetti <andrea@visup.it>
2022-01-13 15:36:56 +0100

    general: add tests

--- projects/ngx-broken-img/src/lib/ngx-broken-img.directive.spec.ts
+++ projects/ngx-broken-img/src/lib/ngx-broken-img.directive.spec.ts
@@ -1,8 +1,35 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { NgxBrokenImgDirective } from './ngx-broken-img.directive';
 
+@Component({
+  template: `
+    <img class="card-img" src="http://www.sdf.com" ngxBrokenImg />`,
+})
+class TestComponent {}
+
 describe('NgxBrokenImgDirective', () => {
+  let elements;
+  let fixture;
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [NgxBrokenImgDirective, TestComponent],
+    }).createComponent(TestComponent);
+    fixture.detectChanges();
+    elements = fixture.debugElement.queryAll(By.directive(NgxBrokenImgDirective));
+  });
+
   it('should create an instance', () => {
     const directive = new NgxBrokenImgDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('broken image should be replaced with placeholder', (done) => {
+    setTimeout(() => {
+      const src = elements[0].nativeElement.src;
+      expect(src).toContain('data:image');
+      done();
+    }, 500);
   });
 });
